351db237cb151a81b7259912166c255c64e5f7d9
diegomata90 <diegomata90@gmail.com>
2023-05-30 16:10:18 -0600

    Se mejora en js de menuresp para que cuando se le de click se oculte el menu responsive

--- static/js/menuresp.js
+++ static/js/menuresp.js
@@ -3,14 +3,26 @@ const enlaces = document.querySelector('.enlace-menu');
 const barras = document.querySelectorAll('.ham span');
 
 
-function activaMenu(){
+function activaMenu() {
     // cambia el display:none al display:flex
     enlaces.classList.toggle('activado');
 
     // para cada elemento de la variable barras de cada elemento span
     // se le agrega la clase animado
-    barras.forEach(child =>{child.classList.toggle('animada')})
+    barras.forEach(child => { child.classList.toggle('animada') })
+
+    // Añade este código para ocultar el menú responsive después de seleccionar una opción
+    if (enlaces.classList.contains('activado')) {
+        enlaces.addEventListener('click', () => {
+            enlaces.classList.remove('activado');
+            barras.forEach(child => {
+                child.classList.remove('animada');
+            });
+        });
+    }
+
 };
 
 
-ham.addEventListener('click',activaMenu);
+ham.addEventListener('click', activaMenu);
+
