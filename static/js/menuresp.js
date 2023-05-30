7b58a02f48f39bc0c07b4986b59101e80654bfc2
diegomata90 <diegomata90@gmail.com>
2023-05-30 16:59:03 -0600

    Cambio en menu y botones

--- static/js/menuresp.js
+++ static/js/menuresp.js
@@ -10,19 +10,16 @@ function activaMenu() {
     // para cada elemento de la variable barras de cada elemento span
     // se le agrega la clase animado
     barras.forEach(child => { child.classList.toggle('animada') })
-
-    // Añade este código para ocultar el menú responsive después de seleccionar una opción
-    if (enlaces.classList.contains('activado')) {
-        enlaces.addEventListener('click', () => {
-            enlaces.classList.remove('activado');
-            barras.forEach(child => {
-                child.classList.remove('animada');
-            });
-        });
-    }
-
 };
 
+function inactivaMenu(){
+    enlaces.classList.remove('activado');
+    barras.forEach(child => {
+        child.classList.remove('animada');
+      });
+}
 
 ham.addEventListener('click', activaMenu);
+enlaces.addEventListener('click', inactivaMenu);
+
 
