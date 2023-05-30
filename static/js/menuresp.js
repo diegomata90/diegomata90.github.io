c87ec3824f88eb1cd42f402c217a552f7674baa0
diegomata90 <diegomata90@gmail.com>
2023-05-29 22:51:01 -0600

    Creación del menu resposive v1

---
+++ static/js/menuresp.js
@@ -0,0 +1,15 @@
+const ham = document.querySelector('.ham');
+const enlaces = document.querySelector('.enlace-menu');
+const barras = document.querySelectorAll('.ham span');
+
+
+function activaMenu(){
+    // cambia el display:none al display:flex
+    enlaces.classList.toggle('activado');
+
+    // para cada elemento de la variable barras de cada elemento span
+    // se le agrega la clase animado
+    barras.forEach(child =>{child.classList.toggle('animada')})
+};
+
+ham.addEventListener('click',activaMenu);
